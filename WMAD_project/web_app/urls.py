930a6e4689698194c70179a7b1e87b2a1ff1d0e3
Toushal <toushal37@gmail.com>
2025-10-10 14:29:15 +0400

    Made a few changes: 1. Added a footer component 2. remove duplicate code (navbar) from pages and made it into its own components 3. made a base.html that have the same code and can be overwriten in other file. also include the components that can are preloaded ising {% include %} 4. made the rest of the pages, pages inherits from base.html and overwrite some lines

--- WMAD_project/web_app/urls.py
+++ WMAD_project/web_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('menu/', views.menu, name='menu'),
+    path('order/', views.order, name='order'),
+    path('reservation/', views.reservation, name='reservation'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- WMAD_project/web_app/views.py
+++ WMAD_project/web_app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'web_app/home.html')
+
+def menu(request):
+    return render(request, 'web_app/menu.html')
+
+def order(request):
+    return render(request, 'web_app/order.html')
+
+def reservation(request):
+    return render(request, 'web_app/reservation.html')
+
+def about(request):
+    return render(request, 'web_app/about.html')
+
+def contact(request):
+    return render(request, 'web_app/contact.html')
