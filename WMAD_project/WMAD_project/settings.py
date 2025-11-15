3857fa8ffe882138243d42945a5214d36e24e990
Isha1233449vn <ishanarain171@gmail.com>
2025-11-15 21:18:55 +0400

    Add dynamic menu page with database-driven items and responsive styling

--- WMAD_project/WMAD_project/settings.py
+++ WMAD_project/WMAD_project/settings.py
@@ -158,3 +158,5 @@ LOGGING = {
         },
     },
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- WMAD_project/web_app/admin.py
+++ WMAD_project/web_app/admin.py
@@ -5,3 +5,8 @@ from django.contrib import admin
 from .models import Special
 
 admin.site.register(Special)
+
+from django.contrib import admin
+from .models import MenuItem
+
+admin.site.register(MenuItem)
