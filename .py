9e8a4fdb86ecea8f71c7c29f7cb8b537ef666969
hayil0511 <hayilnemchand@gmail.com>
2025-11-25 20:10:04 +0400

    Add models.py

---
+++ .py
@@ -0,0 +1,37 @@
+[33m4976fee[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mHayilsing[m[33m)[m Merge branch 'main' of https://github.com/Toushal29/WMAD_Assignment
+[33m5657aa2[m Add models.py
+[33mce00c8c[m Merge branch 'Toushal'
+[33m9d3f0a1[m Made some changes to login page + Made the Signup page     (only front end, no client-side validation)
+[33m046d0c4[m Added content to login page.
+[33m71cfbc5[m some changes + some update, no features added
+[33m31879fa[m remove log panel from dashboard
+[33me7513dd[m 26/10/25-   1. configured and deployed the website on the internet. had to setup a .bat file that will start the server. person with my public ip address and port  can access it.             2. setup a control panel app             3. activate and deactivate the website for maintenance             4. add a .log to view logs in control panel dashboard
+[33m11e61d1[m 1. re-organised the files into their proper folder 2. setup the Privacy Policy page  3. finished coding the PP page  4. made some edits to some files (mostly related to style)
+[33m0ba84b3[m 1. re-organised the files into their proper folder  2. setup the Privacy Policy page  3. finished coding the PP page  4. made some edits to some files (mostly related to style)
+[33m05cbde4[m Merge pull request #5 from Toushal29/Toushal
+[33mb5afd3e[m > made some changes to some files, setup login and signup page-file. content to be coded later.
+[33mccc30c7[m Fixed some bugs
+[33mc6d4c9a[m Merge branch 'Toushal'
+[33mf6ac99b[m 1. Redid some files 2. Merge Contact and About into same page 3. Made the About_Contact page 4. Mine Apollo
+[33mcace99e[m Merge branch 'main' of https://github.com/Toushal29/WMAD_Assignment
+[33m7d2e87f[m Merge branch 'Toushal'- update requirement
+[33md25b543[m Merge pull request #4 from Toushal29/Toushal
+[33m08720f1[m[33m ([m[1;31morigin/Toushal[m[33m)[m update requirements
+[33m7967142[m Merge branch 'Toushal' of https://github.com/Toushal29/WMAD_Assignment
+[33m930a6e4[m Made a few changes: 1. Added a footer component 2. remove duplicate code (navbar) from pages and made it into its own components 3. made a base.html that have the same code and can be overwriten in other file. also include the components that can are preloaded ising {% include %} 4. made the rest of the pages, pages inherits from base.html and overwrite some lines
+[33mdd1d06f[m[33m ([m[1;31morigin/development[m[33m)[m ahhhhh
+[33me62dcfa[m did something today
+[33m13bd7c0[m Merge pull request #3 from Toushal29/development
+[33me8a6a37[m redo everything
+[33m53258d8[m resets
+[33m9ec766a[m Merge pull request #2 from Toushal29/main
+[33m012cd90[m trial commit
+[33me38fe1d[m Merge pull request #1 from Toushal29/main
+[33m70ae35e[m another update
+[33mc397612[m last minute change
+[33m50584c0[m new
+[33m8a5edc9[m change
+[33ma2449b7[m new change
+[33m9833e5f[m new edits
+[33m239ba1b[m Initial Django project setup
+[33m8dfe8b7[m Initial commit
