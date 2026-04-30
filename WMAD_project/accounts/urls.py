150e354ca9478b3574128f50db02ed943f03892e
hayil0511 <hayilnemchand@gmail.com>
2026-04-30 12:41:20 +0400

    feat(auth): update accounts URLs with namespace

--- WMAD_project/accounts/urls.py
+++ WMAD_project/accounts/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = 'accounts'  # ← THIS IS IMPORTANT
 
 urlpatterns = [
-    # Website HTML views
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     
-    # API endpoints for mobile app
+    # API endpoints
     path('api/register/', views.api_register, name='api_register'),
     path('api/login/', views.api_login, name='api_login'),
     path('api/logout/', views.api_logout, name='api_logout'),
